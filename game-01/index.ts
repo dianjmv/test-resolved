f7de186a945547b955c53b3f1e4ced37022db680
Danny <dianjmv@gmail.com>
2021-12-24 01:32:50 -0500

    fix error with indexes and add run script

--- game-01/index.ts
+++ game-01/index.ts
@@ -1,10 +1,14 @@
-const numbers = [2, 5, 8, 14, 0];
-const n = 10;
-function calculate(numbers: number[], total: number): number[]{
-    const subtractionList = numbers.map(number => total - number);
-    return subtractionList.filter(num => {
-        if (numbers.includes(num)) {
-            return [num, numbers.indexOf(num)]
+import {flatMap} from "lodash";
+const numbers = [2, 5, 8, 14, 0, 10, 10];
+const total = 7;
+function calculate(numbers: number[], total: number){
+    const numberSet= new Set()
+    return flatMap(numbers,num =>{
+        const subtractionResult = total - num;
+        if (numberSet.has(subtractionResult)){
+            return [num, subtractionResult]
         }
-    });
+        numberSet.add(num);
+    }).filter(nums => nums !== undefined).slice(0,2)
 }
+console.log(calculate(numbers, total));
